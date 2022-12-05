d1e79a76ad8a00ee1b40924d05ee5c3ad86408a8
Tigran Petrosyan <tigranpetrosyan.707@gmail.com>
2022-12-05 16:50:48 +0400

    added styles in contact and fixed buttons in singlePriduct

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import { Container } from '@mui/material';
+import { Button, Card, CardContent, Grid, TextField } from '@mui/material';
+
+
 
 export default function Contact() {
    return (
-      <Container>
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-         contact contact contact contact contact contact contact contact contact
-      </Container>
+         <Card sx={{maxWidth: 500, margin: "20px auto", padding : "20px 5px"}}>
+        <CardContent>
+         <form>
+         <Grid container spacing={1}>
+            <Grid xs={12} sm={6} item>
+               <TextField color="success" label="First Name" placeholder='Enter your name' fullWidth required/>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+               <TextField color="success" label="Last Name" placeholder='Enter your last name' fullWidth required/>
+            </Grid>
+            <Grid xs={12} item>
+               <TextField color="success" type="email" label="Email" placeholder='Enter email' fullWidth required/>
+            </Grid>
+            <Grid xs={12} item>
+               <TextField color="success" type="tel" label="Phone" placeholder='Enter phone number' fullWidth required/>
+            </Grid>
+            <Grid xs={12} item>
+               <TextField color="success" label="Message" multiline rows={4} placeholder='Type your message here' fullWidth required/>
+            </Grid>
+            <Grid xs={12} item>
+               <Button sx={{background: "rgb(103,173,75)", '&:hover' : {background: "rgba(103,173,75,0.8)"}}} type='submit' variant='contained' fullWidth>Submit</Button>
+            </Grid>
+         </Grid>
+         </form>
+        </CardContent>
+      </Card>
    );
 }
