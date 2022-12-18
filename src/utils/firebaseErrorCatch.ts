26ede1ff113802e4a6d60f0fe12b86b1d370e23a
Suren <sur_zaqaryan@mail.ru>
2022-12-18 13:23:27 +0400

    sign in sign up error catching

---
+++ src/utils/firebaseErrorCatch.ts
@@ -0,0 +1,10 @@
+import { useNotify } from '../hooks/useNotify';
+import { FirebaseErrors, notificationTypes } from '../types';
+
+const notify = useNotify();
+
+export function firebaseErrorCatch(error: string): void {
+   type index = keyof typeof FirebaseErrors;
+   const myKey = error as index;
+   notify(notificationTypes.ERROR, FirebaseErrors[myKey]);
+}
